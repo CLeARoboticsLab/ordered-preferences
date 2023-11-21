fa049c78e04e0752cf74bd416eb9afb9bfcb4aad
lassepe <lasse.peters@mailbox.org>
2023-11-22 00:01:48 +0100

    Update actions and remove footgun default

--- src/parametric_optimization_problem.jl
+++ src/parametric_optimization_problem.jl
@@ -45,7 +45,7 @@ function ParametricOptimizationProblem(;
     objective,
     equality_constraint,
     inequality_constraint,
-    parameter_dimension = 1,
+    parameter_dimension,
     primal_dimension,
     equality_dimension,
     inequality_dimension,
