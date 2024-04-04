b672b3a74010e9b11ad56f311b2c5d1f13f03002
leedh0124 <35527664+leedh0124@users.noreply.github.com>
2024-04-04 16:49:41 -0500

    Cleaned a bit

--- experiments/Lex_QP.jl
+++ experiments/Lex_QP.jl
@@ -9,7 +9,7 @@ function demo(;verbose = false)
     ϵ = 1.0
     κ = 0.1
     max_iterations = 10
-    relaxation_mode = :standard
+    relaxation_mode = :l_infinity
 
     # Lex Min QP Fiaschi (2021)
     Qₒ = [10 -2 4; 
@@ -19,32 +19,30 @@ function demo(;verbose = false)
     cₒ = [-1; -1; 0]
 
     # Reconstruct Q,q,c to match the new primal dimension
+    original_primal_dim = 3 
+    previous_dual_dim = 5
+    primal_dimension = original_primal_dim + previous_dual_dim
+
     if relaxation_mode == :standard
-        Q = zeros(8,8)
-        q = zeros(8)
-        c = zeros(8)
+        Q = zeros(primal_dimension, primal_dimension)
+        q = zeros(primal_dimension)
+        c = zeros(primal_dimension)
+
+    elseif relaxation_mode == :l_infinity
+        Q = zeros(primal_dimension + 1, primal_dimension + 1)
+        q = zeros(primal_dimension + 1)
+        c = zeros(primal_dimension + 1)
     else
-        Q = zeros(9,9)
-        q = zeros(9)
-        c = zeros(9)
+        error("Relaxation mode not supported")
     end
 
-    Q[1:3, 1:3] = Qₒ
-    q[1:3] = qₒ
-    c[1:3] = cₒ
+    Q[1:original_primal_dim, 1:original_primal_dim] = Qₒ
+    q[1:original_primal_dim] = qₒ
+    c[1:original_primal_dim] = cₒ
 
     # Define objective and constraints
     objective(x,θ) = c'x
-    if relaxation_mode == :standard
-        equality_constraints(x,θ) = [
-            Q*x + q - x[4]*[1;-1;-1;zeros(5)] - x[5]*[1;1;-1;zeros(5)] - x[6]*[-1;1;-1;zeros(5)] - x[7]*[-1;-1;-1;zeros(5)] - x[8]*[0;0;1;zeros(5)];
-        ]
-    else
-        equality_constraints_l_inf(x,θ) = [
-            Q*x + q - x[4]*[1;-1;-1;zeros(6)] - x[5]*[1;1;-1;zeros(6)] - x[6]*[-1;1;-1;zeros(6)] - x[7]*[-1;-1;-1;zeros(6)] - x[8]*[0;0;1;zeros(6)];
-        ]
-    end
-    
+
     c1(x,θ) =  x[1] - x[2] - x[3] + 1
     c2(x,θ) =  x[1] + x[2] - x[3] + 1
     c3(x,θ) = -x[1] + x[2] - x[3] + 1
@@ -64,10 +62,15 @@ function demo(;verbose = false)
         -x[7]*c4(x,θ);
         -x[8]*x[3];
     ]
-    primal_dimension = 3 + 5
 
-    
-    MPCC_prob =  ParametricMPCC(; 
+    if relaxation_mode == :standard
+        equality_constraints(x,θ) = [
+            (Q*x + q - x[4]*[1;-1;-1;zeros(previous_dual_dim)] - x[5]*[1;1;-1;zeros(previous_dual_dim)] 
+            - x[6]*[-1;1;-1;zeros(previous_dual_dim)] - x[7]*[-1;-1;-1;zeros(previous_dual_dim)] 
+            - x[8]*[0;0;1;zeros(previous_dual_dim)]);
+        ]
+
+        MPCC_prob =  ParametricMPCC(; 
         objective = objective,
         equality_constraints = equality_constraints,
         inequality_constraints = inequality_constraints,
@@ -79,7 +82,29 @@ function demo(;verbose = false)
         max_iterations = max_iterations,
         relaxation_mode = relaxation_mode,
     )
+    elseif relaxation_mode == :l_infinity
+        equality_constraints_l_inf(x,θ) = [
+            (Q*x + q - x[4]*[1;-1;-1;zeros(previous_dual_dim);zeros(1)] - x[5]*[1;1;-1;zeros(previous_dual_dim);zeros(1)] 
+            - x[6]*[-1;1;-1;zeros(previous_dual_dim);zeros(1)] - x[7]*[-1;-1;-1;zeros(previous_dual_dim);zeros(1)] 
+            - x[8]*[0;0;1;zeros(previous_dual_dim);zeros(1)]);
+        ]
 
+        MPCC_prob =  ParametricMPCC(; 
+        objective = objective,
+        equality_constraints = equality_constraints_l_inf,
+        inequality_constraints = inequality_constraints,
+        complementarity_constraints = complementarity_constraints,
+        primal_dimension = primal_dimension,
+        parameter_dimension = 1,
+        relaxation_parameter = ϵ,
+        update_parameter = κ,
+        max_iterations = max_iterations,
+        relaxation_mode = relaxation_mode,
+    )
+    else
+        error("Relaxation mode not supported")
+    end
+    
     # Solve MPCC
     tolerance = 1e-7
     parameters = zeros(MPCC_prob.subproblems[1].parameter_dimension)
