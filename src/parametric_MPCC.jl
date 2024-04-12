c7fa502e62941c214174a14ac0ecff7fe4dfb8eb
lassepe <lasse.peters@mailbox.org>
2024-04-12 20:06:51 +0200

    --wip-- [skip ci]

--- src/parametric_MPCC.jl
+++ src/parametric_MPCC.jl
@@ -1,6 +1,6 @@
-struct ParametricMPCC{T1<:Vector{<:ParametricOptimizationProblem}}
-    subproblems::T1
-    complementarity_dimension::Int
+struct ParametricMPCC{T1}
+    "The MPCC reformulated as regular optimization problem with an additional runtime parameter for relaxation"
+    relaxed_parametric_optimization_problem::T1
 end
 
 """
@@ -12,91 +12,104 @@ function ParametricMPCC(;
     equality_constraints,
     inequality_constraints,
     complementarity_constraints, # Φ(Gᵢ(x), Hᵢ(x)) ≤ 0, i = 1,...,m
-    primal_dimension,   
+    primal_dimension,
     parameter_dimension,
-    relaxation_parameter,
+    relaxation_parameter_schedule = [1.0, 0.1, 0.01, 0.001],
     update_parameter,
     max_iterations,
     relaxation_mode = :standard,
-    )
+)
+    augmented_parameter_dimension = parameter_dimension + 1 # +1 for relaxation parameter
 
     @assert 0.0 < update_parameter < 1.0 "Update parameter must be in the range (0,1)"
 
     # Problem data
     dummy_parameters = zeros(parameter_dimension)
 
-    subproblems = (ParametricOptimizationProblem[])
-    
-    # First subproblem is the original MPCC
-    relaxations = vcat([0], relaxation_parameter * update_parameter.^(0:max_iterations))
-    
     # Set relaxation mode
-    if relaxation_mode === :standard
-        dummy_primals = zeros(primal_dimension)
+    #    if relaxation_mode === :standard
+    dummy_primals = zeros(primal_dimension)
 
-        equality_dimension = length(equality_constraints(dummy_primals, dummy_parameters))
-        complementarity_dimension = length(complementarity_constraints(dummy_primals, dummy_parameters))
-        inequality_dimension = length(inequality_constraints(dummy_primals, dummy_parameters)) + complementarity_dimension
+    equality_dimension = length(equality_constraints(dummy_primals, dummy_parameters))
+    complementarity_dimension =
+        length(complementarity_constraints.H(dummy_primals, dummy_parameters))
+    @assert length(complementarity_constraints.G(dummy_primals, dummy_parameters)) ==
+            complementarity_dimension
 
-        objective_ϵ = objective
-        for ϵ in relaxations
-            # Same scheduling of relaxation parameters for all levels 
-            ϵ_inner = ϵ*ones(parameter_dimension) # [ϵ₁,...,ϵₖ]
-            combined_inequality_constraints = function (x,ϵ_inner) 
-                [inequality_constraints(x,ϵ_inner); complementarity_constraints(x,ϵ_inner) .+ ϵ]
-            end
-            relaxed_problem = ParametricOptimizationProblem(;
-                objective = objective_ϵ,
-                equality_constraint = equality_constraints,
-                inequality_constraint = combined_inequality_constraints,
-                parameter_dimension = parameter_dimension,
-                primal_dimension = primal_dimension,   
-                equality_dimension = equality_dimension,
-                inequality_dimension = inequality_dimension,
-            )
+    augmented_inequality_dimension =
+        length(inequality_constraints(dummy_primals, dummy_parameters)) +
+        3 * complementarity_dimension # additional constraints for G(x) >= 0, H(x) >= 0, G(x) .* H(x) <= ϵ
 
-            push!(subproblems, relaxed_problem)
-        end
+    # Same scheduling of relaxation parameters for all levels
+    combined_inequality_constraints = function (x, θ_augmented)
+        θ = θ_augmented[1:parameter_dimension]
+        ϵ = θ_augmented[parameter_dimension + 1]
 
-    elseif relaxation_mode === :l_infinity
-        primal_dimension = primal_dimension + 1
-        dummy_primals = zeros(primal_dimension)
-
-        equality_dimension = length(equality_constraints(dummy_primals, dummy_parameters))
-        complementarity_dimension = length(complementarity_constraints(dummy_primals, dummy_parameters))
-        inequality_dimension = length(inequality_constraints(dummy_primals, dummy_parameters)) + complementarity_dimension
-
-        for ϵ in relaxations
-            ϵ_inner = ϵ*ones(parameter_dimension)
-            if isequal(ϵ, 0.0)
-                objective_ϵ = objective
-                combined_inequality_constraints = function (x,θ)
-                    [inequality_constraints(x,θ); complementarity_constraints(x,θ)]
-                end
-            else
-                objective_ϵ = function (x,θ)  
-                    objective(x,θ) +  x[primal_dimension] / ϵ
-                end
-                combined_inequality_constraints = function (x,ϵ_inner)
-                    [inequality_constraints(x,ϵ_inner); complementarity_constraints(x,ϵ_inner) .+ x[primal_dimension]]
-                end
-            end
-            inequality_dimension = length(combined_inequality_constraints(dummy_primals, dummy_parameters))
-            relaxed_problem = ParametricOptimizationProblem(;
-                objective = objective_ϵ,
-                equality_constraint = equality_constraints,
-                inequality_constraint = combined_inequality_constraints,
-                parameter_dimension = parameter_dimension,
-                primal_dimension = primal_dimension,   
-                equality_dimension = equality_dimension,
-                inequality_dimension = inequality_dimension,
-            )
-
-            push!(subproblems, relaxed_problem)
-        end
-    else
-        error("Invalid relaxation mode")
+        Gxθ = complementarity_constraints.G(x, θ)
+        Hxθ = complementarity_constraints.H(x, θ)
+        [
+            inequality_constraints(x, θ)
+            Gxθ
+            Hxθ
+            -Gxθ .* Hxθ .+ ϵ
+        ]
     end
+
+    relaxed_parametric_optimization_problem = ParametricOptimizationProblem(;
+        objective,
+        equality_constraint = equality_constraints,
+        inequality_constraint = combined_inequality_constraints,
+        parameter_dimension = augmented_parameter_dimension,
+        primal_dimension = primal_dimension,
+        equality_dimension = equality_dimension,
+        inequality_dimension = augmented_inequality_dimension,
+    )
+    #    elseif relaxation_mode === :l_infinity
+    #        primal_dimension = primal_dimension + 1
+    #        dummy_primals = zeros(primal_dimension)
+    #
+    #        equality_dimension = length(equality_constraints(dummy_primals, dummy_parameters))
+    #        complementarity_dimension =
+    #            length(complementarity_constraints(dummy_primals, dummy_parameters))
+    #        inequality_dimension =
+    #            length(inequality_constraints(dummy_primals, dummy_parameters)) +
+    #            complementarity_dimension
+    #
+    #        for ϵ in relaxations
+    #            ϵ_inner = ϵ * ones(parameter_dimension)
+    #            if isequal(ϵ, 0.0)
+    #                objective_ϵ = objective
+    #                combined_inequality_constraints = function (x, θ)
+    #                    [inequality_constraints(x, θ); complementarity_constraints(x, θ)]
+    #                end
+    #            else
+    #                objective_ϵ = function (x, θ)
+    #                    objective(x, θ) + x[primal_dimension] / ϵ
+    #                end
+    #                combined_inequality_constraints = function (x, ϵ_inner)
+    #                    [
+    #                        inequality_constraints(x, ϵ_inner)
+    #                        complementarity_constraints(x, ϵ_inner) .+ x[primal_dimension]
+    #                    ]
+    #                end
+    #            end
+    #            inequality_dimension =
+    #                length(combined_inequality_constraints(dummy_primals, dummy_parameters))
+    #            relaxed_problem = ParametricOptimizationProblem(;
+    #                objective = objective_ϵ,
+    #                equality_constraint = equality_constraints,
+    #                inequality_constraint = combined_inequality_constraints,
+    #                parameter_dimension = parameter_dimension,
+    #                primal_dimension = primal_dimension,
+    #                equality_dimension = equality_dimension,
+    #                inequality_dimension = inequality_dimension,
+    #            )
+    #
+    #            push!(subproblems, relaxed_problem)
+    #        end
+    #    else
+    #        error("Invalid relaxation mode")
+    #    end
 
     ParametricMPCC(subproblems, complementarity_dimension)
 end
@@ -107,13 +120,13 @@ end
 
 function solve_relaxed_mpcc(
     problem::ParametricMPCC,
-    initial_guess::Union{Nothing, Vector{Float64}},
+    initial_guess::Union{Nothing,Vector{Float64}},
     parameters;
     ϵ = 1.0,
     κ = 0.1,
     max_iterations = 10,
     tolerance = 1e-7,
-    verbose = false
+    verbose = false,
 )
     solutions = []
 
@@ -128,26 +141,32 @@ function solve_relaxed_mpcc(
     complementarity_residual = 1.0
 
     # Start from the second subproblem: First subproblem is the original MPCC
-    ii = 2 
+    ii = 2
     while complementarity_residual > tolerance && ii ≤ max_iterations + 2
 
         # If the relaxed problem is infeasible, terminate. Otherwise solve the relaxed problem for xᵏ⁺¹
         relaxed_problem = problem.subproblems[ii]
         solution = solve(relaxed_problem, parameters; initial_guess)
         if verbose
-            println("ii: ", ii-1)
+            println("ii: ", ii - 1)
             println("status: ", solution.status)
             println("primals: ", solution.primals)
             println("objective: ", original_problem.objective(solution.primals, parameters))
         end
 
-        if string(solution.status) != "MCP_Solved" 
-            verbose && printstyled("Could not solve relaxed problem at relaxation factor $(ii-1).\n"; color = :red)
+        if string(solution.status) != "MCP_Solved"
+            verbose && printstyled(
+                "Could not solve relaxed problem at relaxation factor $(ii-1).\n";
+                color = :red,
+            )
             break
         end
 
         # Check complementarity residual
-        complementarity_violations = last(original_problem.inequality_constraint(solution.primals, parameters), complementarity_dimension)
+        complementarity_violations = last(
+            original_problem.inequality_constraint(solution.primals, parameters),
+            complementarity_dimension,
+        )
         complementarity_residual = findmax(-complementarity_violations)[1]
 
         # Stop if iteration does not improve. Otherwise update initial_guess
@@ -155,7 +174,7 @@ function solve_relaxed_mpcc(
             verbose && printstyled("Converged at iteration $(ii-1).\n"; color = :green)
             break
         else
-            initial_guess = solution.variables 
+            initial_guess = solution.variables
             push!(solutions, solution)
         end
 
@@ -166,5 +185,4 @@ function solve_relaxed_mpcc(
 
     verbose && println("complementarity_residual: ", complementarity_residual)
     (; relaxation = ϵ, solution = solutions[end], residual = complementarity_residual)
-end 
-
+end
--- src/parametric_ordered_preferences_MPCC.jl
+++ src/parametric_ordered_preferences_MPCC.jl
@@ -19,14 +19,14 @@ function ParametricOrderedPreferencesMPCC(;
     relaxation_parameter,
     update_parameter,
     max_iterations,
-    relaxation_mode = :standard, 
+    relaxation_mode = :standard,
 )
     # Problem data
     ordered_priority_levels = eachindex(prioritized_preferences)
 
     dual_dimension = 0
 
-    inner_equality_constraints = Function[equality_constraints] 
+    inner_equality_constraints = Function[equality_constraints]
     inner_inequality_constraints = Function[inequality_constraints]
     complementarity_constraints = Function[]
 
@@ -34,9 +34,15 @@ function ParametricOrderedPreferencesMPCC(;
     equality_dimension_ii = equality_dimension
 
     function set_up_level(priority_level)
+        is_inner_most_problem = priority_level == 1
+        if is_inner_most_problem
+            augmented_parameter_dimension = parameter_dimension
+        else
+            augmented_parameter_dimension = parameter_dimension + 1 # one extra parameter for relaxation
+        end
+
         #TODO: Implement priority objective (for now) vs priority constraints 
         primal_dimension_ii = primal_dimension + dual_dimension
-
         priority_preferences_ii = prioritized_preferences[priority_level]
 
         # Define symbolic variables for primals.
@@ -56,51 +62,55 @@ function ParametricOrderedPreferencesMPCC(;
         end
 
         # Build symbolic expression for objective and constraints. 
-        objective_ii = priority_preferences_ii(x, θ) 
-        
+        objective_ii = priority_preferences_ii(x, θ)
+
         g_ii = mapreduce(vcat, inner_equality_constraints) do constraint
-                constraint(x,θ)
-            end
+            constraint(x, θ)
+        end
         if isempty(g_ii)
             g_ii = Symbolics.Num[]
         end
 
         h_ii = mapreduce(vcat, inner_inequality_constraints) do constraint
-                constraint(x,θ)
-            end
+            constraint(x, θ)
+        end
         if isempty(h_ii)
             h_ii = Symbolics.Num[]
         end
 
         # Lagrangian.
-        L = objective_ii - λ' * h_ii + μ' * g_ii 
+        L = objective_ii - λ' * h_ii + μ' * g_ii
 
         # Stationary constraints (# ∇ₓL = 0).
-        stationarity = Symbolics.gradient(L, x) 
+        stationarity = Symbolics.gradient(L, x)
 
         # Concatenate stationary constraint into equality constraints.
-        callable_stationarity = Symbolics.build_function(stationarity, z̃, θ,  expression=Val{false})[1]
+        callable_stationarity =
+            Symbolics.build_function(stationarity, z̃, θ, expression = Val{false})[1]
         push!(inner_equality_constraints, callable_stationarity)
 
         # Dual nonnegativity constraints
         dual_nonnegativity = λ
 
         # Concatenate dual_nonnegativity constraints into inequality constraints.
-        callable_dual_nonnegativity = Symbolics.build_function(dual_nonnegativity, z̃, θ, expression=Val{false})[1]
+        callable_dual_nonnegativity =
+            Symbolics.build_function(dual_nonnegativity, z̃, θ, expression = Val{false})[1]
         push!(inner_inequality_constraints, callable_dual_nonnegativity)
 
         # Complementarity constraints from inequality constraints.
         complementarity = -h_ii .* λ
-  
+
         # Reduce relaxed complementarity_constraints into a single inequality and concatenate.
         if priority_level < last(ordered_priority_levels)
             relaxed_complementarity = sum(complementarity) + θ[priority_level] #TODO: Is this right?
-            callable_complementarity = Symbolics.build_function(relaxed_complementarity, z̃, θ, expression=Val{false})
+            callable_complementarity =
+                Symbolics.build_function(relaxed_complementarity, z̃, θ, expression = Val{false})
             push!(inner_inequality_constraints, callable_complementarity)
         else
             # Final level: relaxation happens in MPCC 
             relaxed_complementarity = complementarity
-            callable_complementarity = Symbolics.build_function(relaxed_complementarity, z̃, θ, expression=Val{false})[1]
+            callable_complementarity =
+                Symbolics.build_function(relaxed_complementarity, z̃, θ, expression = Val{false})[1]
             push!(complementarity_constraints, callable_complementarity)
         end
 
@@ -108,24 +118,23 @@ function ParametricOrderedPreferencesMPCC(;
         dual_dimension += inequality_dimension_ii + equality_dimension_ii
         inequality_dimension_ii += length(dual_nonnegativity) + length(relaxed_complementarity)
         equality_dimension_ii += length(stationarity)
-
     end
 
     for priority_level in ordered_priority_levels
         set_up_level(priority_level)
     end
-    
+
     primal_dimension = primal_dimension + dual_dimension
 
-    equality_constraints = function(x,θ)
+    equality_constraints = function (x, θ)
         mapreduce(vcat, inner_equality_constraints) do constraint
-            constraint(x,θ)
+            constraint(x, θ)
         end
     end
 
-    inequality_constraints = function(x,θ)
+    inequality_constraints = function (x, θ)
         mapreduce(vcat, inner_inequality_constraints) do constraint
-            constraint(x,θ)
+            constraint(x, θ)
         end
     end
 
@@ -134,12 +143,11 @@ function ParametricOrderedPreferencesMPCC(;
         equality_constraints,
         inequality_constraints,
         complementarity_constraints = complementarity_constraints[1], # Φ(Gᵢ(x), Hᵢ(x)) ≤ 0, i = 1,...,m
-        primal_dimension,   
-        parameter_dimension,
+        primal_dimension,
+        parameter_dimension = parameter_dimension + 1, # +1 for relaxation parameter
         relaxation_parameter,
         update_parameter,
         max_iterations,
         relaxation_mode,
     )
 end
-
